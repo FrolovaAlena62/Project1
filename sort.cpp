6e82e42f96669867df092a8ab387d2851e944c49
FrolovaAlena62 <81227155+FrolovaAlena62@users.noreply.github.com>
2021-03-29 19:53:42 +0300

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int arraysize;
+    std::cout << "Enter array size "; // ¬вод массива и его размер с клавиатуры 
+    std::cin >> arraysize;
+    std::vector<int> array(arraysize);
+    std::cout << "Enter " << arraysize << " value's ";
+for (int i = 0; i < arraysize; ++i) {
+    std::cin >> array[i];
+}
+    sort(array.begin(), array.end());;      // функци€ сортировки по возрастанию из библиотеки algorithm
+    for (int ix = 0; ix < arraysize; ix++)
+        std::cout << array[ix] << " ";
+    return 0;
+}
