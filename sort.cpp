dbb52ed8ccbf39566285b070eddb65df00869f1d
FrolovaAlena62 <81227155+FrolovaAlena62@users.noreply.github.com>
2021-04-03 14:39:53 +0300

    Исправила сортировку

--- sort.cpp
+++ sort.cpp
@@ -5,17 +5,29 @@
 
 using namespace std;
 
+void MySort(int inArraySize, int* inArray) {
+    for (int i = 0; i < inArraySize - 1; i++) {
+        for (int j = 0; j < inArraySize - i - 1; j++) {
+            if (inArray[j] > inArray[j + 1]) {
+                // меняем элементы местами
+                int temp = inArray[j];
+                inArray[j] = inArray[j + 1];
+                inArray[j + 1] = temp;
+            }
+        }
+    }
+}
 int main()
 {
     int arraysize;
-    std::cout << "Enter array size "; // Ввод массива и его размер с клавиатуры
+    std::cout << "Enter array size "; 
     std::cin >> arraysize;
     std::vector<int> array(arraysize);
     std::cout << "Enter " << arraysize << " value's ";
-for (int i = 0; i < arraysize; ++i) {
-    std::cin >> array[i];
-}
-    sort(array.begin(), array.end());;      // функция сортировки по возрастанию из библиотеки algorithm
+    for (int i = 0; i < arraysize; ++i) {
+        std::cin >> array[i];
+    }
+    MySort(arraysize, data(array));      
     for (int ix = 0; ix < arraysize; ix++)
         std::cout << array[ix] << " ";
     return 0;
