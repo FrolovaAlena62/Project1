79e763d47ca51a95f0e488807f9a16cd36383fb5
FrolovaAlena62 <81227155+FrolovaAlena62@users.noreply.github.com>
2021-04-05 17:15:55 +0300

    Внесла изменения в сортировку
    Алгоритм сортировки методом выборки по убыванию, а также возможность ввода массива произвольной длины с клавиатуры. Я не смогла придумать, как обойтись без функции data().

--- sort.cpp
+++ sort.cpp
@@ -5,30 +5,29 @@
 
 using namespace std;
 
-void MySort(int inArraySize, int* inArray) {
-    for (int i = 0; i < inArraySize - 1; i++) {
-        for (int j = 0; j < inArraySize - i - 1; j++) {
-            if (inArray[j] > inArray[j + 1]) {
-                // меняем элементы местами
-                int temp = inArray[j];
-                inArray[j] = inArray[j + 1];
-                inArray[j + 1] = temp;
-            }
+void MySort(int inSize, int* inArray) {
+    for (int i = 0; i <= inSize - 2; ++i) {
+        for (int j = 1; j <= inSize - 1 - i; ++j) {
+            if (inArray[i + j] > inArray[i])
+                std::swap(inArray[i + j], inArray[i]);
         }
     }
 }
+
 int main()
 {
+    int i;
     int arraysize;
-    std::cout << "Enter array size "; 
-    std::cin >> arraysize;
-    std::vector<int> array(arraysize);
-    std::cout << "Enter " << arraysize << " value's ";
-    for (int i = 0; i < arraysize; ++i) {
-        std::cin >> array[i];
+    std::vector<int> MyArray;
+    std::cout << "Enter value's (enter 'stop' if you end) ";
+    while (std::cin >> i) {
+        MyArray.push_back(i);
+        if (i == char("stop"))
+            break;
     }
-    MySort(arraysize, data(array));      
+    arraysize = size(MyArray);
+    MySort(arraysize, MyArray.data());
     for (int ix = 0; ix < arraysize; ix++)
-        std::cout << array[ix] << " ";
+        std::cout << MyArray[ix] << " ";
     return 0;
 }
